601d77848fef76bcab03095d71ca6db5f7664bf4
markwasan <jojpo02@gmail.com>
2023-06-28 14:05:43 +0700

    user

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from '../user';
 
 @Component({
   selector: 'app-user-login',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./user-login.component.css']
 })
 export class UserLoginComponent {
-
+user:User = new User();
 }
